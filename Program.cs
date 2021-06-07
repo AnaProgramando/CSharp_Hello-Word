2d0a04a8608deeac108477b99c7a28af82a50b1b
Ana Valentim <31097110+AnaProgramando@users.noreply.github.com>
2021-06-07 15:54:39 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto - Hello Word: \r\n");
+
+            Console.WriteLine("Olá, mundo!");
+            Console.WriteLine("Esse é um Projeto no Visual Studio.");
+            Console.WriteLine("A execução acabou!");
+            Console.WriteLine("Tecle enter para finalizar.");
+            Console.ReadLine();
+        }
+    }
+}
